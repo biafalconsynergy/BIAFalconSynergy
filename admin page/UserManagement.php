4bbb4a7e32d44e30ec7f01ddfefce4fd5b099e5d
gmantillabia <g_mantillaramos@fanshaweonline.ca>
2024-07-02 22:13:01 -0400

    Admin panel

---
+++ admin page/UserManagement.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Management</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+
+    <!--PHP section user management starts-->|
+    <?php
+
+    //Credentials to stablish the connection
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "capstonetest";
+
+    //Connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    //Connection error handling
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    //Creates the query
+    $query = "SELECT id, name FROM users ORDER BY id ASC";
+    $result = $conn->query($query);
+
+    //Creates the table to show the results
+    echo '<table class="table table-hover">
+    <thead>
+     <tr>
+      <th scope="col">id</th>
+      <th scope="col">name</th>
+      <th scope="col">Edit</th>
+     </tr>
+    </thead>
+    <tbody>';
+
+    //Shows the result in the table previously created
+    while ($row = $result->fetch_assoc()) {
+        $field1name = $row['id'];
+        $field2name = $row['name'];
+
+    echo '<tr>
+            <td>'.$field1name.'</td>
+            <td>'.$field2name.'</td>
+            <td><a href="#">Edit</a></td>
+          </tr>';
+    }
+
+    echo '</tbody>
+        </table>';
+
+    //deletes the information stored in $result
+    $result->free();
+
+    //Close the connection
+    $conn->close();
+    ?>
+    <!--PHP section ends-->
+</body>
+</html>
---
+++ admin page/connection.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connection</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php
+
+//Credentials to stablish the connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "capstonetest";
+
+//Connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Connection error handling
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+</body>
+</html>
