719f3bc2be1590af8cd3ffd1f9cb291ce4431088
minolisharmila <31173482+minolisharmila@users.noreply.github.com>
2024-07-07 20:19:37 -0500

    Updated Case_Studies File
    separated header and footer content into separate files

--- case_studies.php
+++ case_studies.php
@@ -66,70 +66,8 @@ $(document).ready(function() {
 </head>
 
 <body>
-    <header>
-        <!--Navigation Pane-->
-        <nav class="navbar navbar-expand-lg bg-light" aria-label="Main Navigation">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <img src="konnexio-logo.png" alt="Konnexio Logo" class="rounded img-fluid" loading="lazy"
-                        typeof="foaf:Image">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapsibleNavbar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">Solutions</a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">adapto™</a></li>
-                                <li><a class="dropdown-item" href="#">Custom Machines</a></li>
-                                <li><a class="dropdown-item" href="#">Case Studies</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">Services</a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Design</a></li>
-                                <li><a class="dropdown-item" href="#">Build</a></li>
-                                <li><a class="dropdown-item" href="#">Support</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">Industries</a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Industrial</a></li>
-                                <li><a class="dropdown-item" href="#">Agriculture</a></li>
-                                <li><a class="dropdown-item" href="#">Hydrogen Fuel Cells</a></li>
-                                <li><a class="dropdown-item" href="#">Other Industries</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">News & Events</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">About</a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Company Story</a></li>
-                                <li><a class="dropdown-item" href="#">Our People</a></li>
-                                <li><a class="dropdown-item" href="#">Associations</a></li>
-                                <li><a class="dropdown-item" href="#">Testimonials</a></li>
-                                <li><a class="dropdown-item" href="#">Careers</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <button class="btn btn-dark btn-lg">Contact</button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+	<!--Header Data-->
+    <?php include 'header.php'; ?>
 
     <main>
         <!--Third Section (Image Containers)-->
@@ -187,70 +125,8 @@ $(document).ready(function() {
         </div>
     </main>
 
-    <!-- Footer Section -->
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-4 footer-section">
-                    <div class="footer-logo">
-                        <img src="konnexio-logo-white.png" alt="Konnexio Logo" class="rounded img-fluid">
-                    </div>
-                    <p><i class="bi bi-envelope-fill"></i> info@konnexio.com</p>
-                    <p><i class="bi bi-telephone-fill"></i> +1 (519) 457-8836</p>
-                    <p><i class="bi bi-geo-alt-fill"></i> 70 Pacific Court, Unit 14<br>London, ON N5V 3R5</p>
-                    <div class="d-flex">
-                        <a class="f-icon" href="https://www.facebook.com/konnexioinc"><i class="bi bi-facebook"></i></a>
-                        <a class="f-icon" href="https://www.youtube.com/@konnexio5801/videos"><i
-                                class="bi bi-youtube"></i></a>
-                        <a class="f-icon" href="https://twitter.com/konnexio"><i class="bi bi-twitter-x"></i></a>
-                        <a class="f-icon"
-                            href="https://www.linkedin.com/company/konnexio-inc-/"><i
-                                class="bi bi-linkedin"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-4 footer-section">
-                    <h5 class="footer-heading"><a href="">About</a></h5>
-                    <ul class="list-unstyled">
-                        <li><a href="#">Company Story</a></li>
-                        <li><a href="#">Our People</a></li>
-                        <li><a href="#">Associations</a></li>
-                        <li><a href="#">Testimonials</a></li>
-                        <li><a href="#">Careers</a></li>
-                    </ul>
-                    <h5 class="footer-heading"><a href="#">News and Events</a></h5>
-                    <h5 class="footer-heading"><a href="#">Contact Us</a></h5>
-                </div>
-                <div class="col-lg-2 col-md-4 footer-section">
-                    <h5 class="footer-heading"><a href="">Solutions</a></h5>
-                    <ul class="list-unstyled">
-                        <li><a href="#">adapto™</a></li>
-                        <li><a href="#">Custom Machines</a></li>
-                        <li><a href="#">Case Studies</a></li>
-                    </ul>
-                </div>
-                <div class="col-lg-2 col-md-4 footer-section">
-                    <h5 class="footer-heading"><a href="">Services</a></h5>
-                    <ul class="list-unstyled">
-                        <li><a href="#">Design</a></li>
-                        <li><a href="#">Build</a></li>
-                        <li><a href="#">Support</a></li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-md-4 footer-section">
-                    <h5 class="footer-heading"><a href="#">Industries</a></h5>
-                    <ul class="list-unstyled">
-                        <li><a href="#">Industrial</a></li>
-                        <li><a href="#">Agriculture</a></li>
-                        <li><a href="#">Hydrogen Fuel Cells</a></li>
-                        <li><a href="#">Other Industries</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="text-center mt-2">
-                <p>© 2024 Konnexio Inc. All Rights Reserved.</p>
-            </div>
-        </div>
-    </footer>
+  <!--footer data -->
+  <?php include 'footer.php'; ?>  
 
 </body>
 
---
+++ footer.php
@@ -0,0 +1,63 @@
+<footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-4 footer-section">
+                    <div class="footer-logo">
+                        <img src="konnexio-logo-white.png" alt="Konnexio Logo" class="rounded img-fluid">
+                    </div>
+                    <p><i class="bi bi-envelope-fill"></i> info@konnexio.com</p>
+                    <p><i class="bi bi-telephone-fill"></i> +1 (519) 457-8836</p>
+                    <p><i class="bi bi-geo-alt-fill"></i> 70 Pacific Court, Unit 14<br>London, ON N5V 3R5</p>
+                    <div class="d-flex">
+                        <a class="f-icon" href="https://www.facebook.com/konnexioinc"><i class="bi bi-facebook"></i></a>
+                        <a class="f-icon" href="https://www.youtube.com/@konnexio5801/videos"><i
+                                class="bi bi-youtube"></i></a>
+                        <a class="f-icon" href="https://twitter.com/konnexio"><i class="bi bi-twitter-x"></i></a>
+                        <a class="f-icon"
+                            href="https://www.linkedin.com/company/konnexio-inc-/"><i
+                                class="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 footer-section">
+                    <h5 class="footer-heading"><a href="">About</a></h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Company Story</a></li>
+                        <li><a href="#">Our People</a></li>
+                        <li><a href="#">Associations</a></li>
+                        <li><a href="#">Testimonials</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
+                    <h5 class="footer-heading"><a href="#">News and Events</a></h5>
+                    <h5 class="footer-heading"><a href="#">Contact Us</a></h5>
+                </div>
+                <div class="col-lg-2 col-md-4 footer-section">
+                    <h5 class="footer-heading"><a href="">Solutions</a></h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">adapto™</a></li>
+                        <li><a href="#">Custom Machines</a></li>
+                        <li><a href="#">Case Studies</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-md-4 footer-section">
+                    <h5 class="footer-heading"><a href="">Services</a></h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Design</a></li>
+                        <li><a href="#">Build</a></li>
+                        <li><a href="#">Support</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-4 footer-section">
+                    <h5 class="footer-heading"><a href="#">Industries</a></h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Industrial</a></li>
+                        <li><a href="#">Agriculture</a></li>
+                        <li><a href="#">Hydrogen Fuel Cells</a></li>
+                        <li><a href="#">Other Industries</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="text-center mt-2">
+                <p>© 2024 Konnexio Inc. All Rights Reserved.</p>
+            </div>
+        </div>
+</footer>
