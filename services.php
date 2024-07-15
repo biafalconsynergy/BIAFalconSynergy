f8f657481a716636d16ba5c1f76c526bf9a5a727
felicity-S <felishaspurthi@gmail.com>
2024-07-15 03:46:21 -0400

    Update services.php

--- services.php
+++ services.php
@@ -21,13 +21,96 @@
 </head>
 
 <body>
-    
     <!-- Header Section -->
     <?php include 'header.php'; ?>
 
+    <!-- Services -->
+  <section class="services-section">
+    <div class="container my-5">
+      <h1>Services</h1>
+    </div>
+  </section>
 
-     <!-- Footer Section -->
-     <?php include 'footer.php'; ?>
+    <!-- Design -->
+    <div class="container mt-5">
+        <h1 class="text-center">Design</h1>
+        <div class="row mt-4 justify-content-center">
+            <div class="col-md-3 mb-4">
+                <div class="card service-card">
+                    <img src="Images/Image6c.jpg" class="card-img-top" alt="Mechanical Design">
+                </div>
+                <h5 class="card-title">Mechanical Design</h5>
+                <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card service-card">
+                    <img src="Images/Image6b.jpg" class="card-img-top" alt="Electronics Design">
+                </div>
+                <h5 class="card-title">Electronics Design</h5>
+                <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card service-card">
+                    <img src="Images/Image7c.jpg" class="card-img-top" alt="Electrical Design">
+                </div>
+                <h5 class="card-title">Electrical Design</h5>
+                <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card service-card">
+                    <img src="Images/Image2a.jpg" class="card-img-top" alt="Industrial Design">
+                </div>
+                <h5 class="card-title">Industrial Design</h5>
+                <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+        </div>
+    </div>
 
+     <!-- Build -->
+
+     <div class="container mt-5">
+        <h1 class="text-center">Build</h1>
+        <div class="row mt-4 justify-content-center">
+            <div class="col-md-3 mb-4">
+                <div class="card service-card">
+                    <img src="Images/Image4b.jpg" class="card-img-top" alt="Industrial Automation">
+                </div>
+                <h5 class="card-title">Industrial Automation & Robotics Engineering </h5>
+                <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card service-card">
+                    <img src="Images/Image4c.jpg" class="card-img-top" alt="afety Engineering">
+                </div>
+                <h5 class="card-title">Safety Engineering</h5>
+                <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card service-card">
+                    <img src="Images/Image7b.jpg" class="card-img-top" alt="Custom Machine">
+                </div>
+                <h5 class="card-title">Custom Machine Building</h5>
+                <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Support -->
+
+    <div class="container mt-5">
+        <h1 class="text-center">Support</h1>
+        <div class="row mt-4 justify-content-center">
+            <div class="col-md-3 mb-4">
+                <div class="card service-card">
+                    <img src="Images/Image4a.jpg" class="card-img-top" alt="Machine Maintenance">
+                </div>
+                <h5 class="card-title">Machine Maintenance</h5>
+                <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+            </div>
+            </div>
+    <!-- Footer Section -->
+    <?php include 'footer.php'; ?>
 </body>
+
 </html>
