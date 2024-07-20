861fbbc7b4f01c9231e86786ee6c5b7f575dcc54
gmantillabia <g_mantillaramos@fanshaweonline.ca>
2024-07-19 20:49:47 -0400

    session start fixed
    <?php if (session_status() === PHP_SESSION_NONE) {
                session_start();
            }
    ?>

---
+++ admin_page/ReferencesTop.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>References Top</title>
+</head>
+<body>
+    <!-- css reference -->
+    <link rel="stylesheet" href="styles.css">
+
+    <!-- bootstrap reference -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Template source: https://startbootstrap.com/theme/sb-admin-2 -->
+    
+    <!-- Custom fonts for this template From: Template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template From: Template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page From: Template-->
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+</body>
+</html>
+
