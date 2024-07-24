622f3d3b9cb7eff5709d3b808ab846e147a88ba9
Saikat Datta <sakatdata@gmail.com>
2024-07-24 02:16:19 -0400

    Fixed cards section for responsive view

--- index.php
+++ index.php
@@ -81,7 +81,7 @@
         <!--Second Section (Cards)-->
         <section id="cardsSection" class="container-fluid" style="padding: 0;">
             <div class="row no-gutters">
-                <div class="col-sm-3">
+                <div class="col-12 col-sm-6 col-md-3">
                     <div class="card">
                         <div class="card-inner">
                             <div class="card-front">
@@ -100,7 +100,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-3">
+                <div class="col-12 col-sm-6 col-md-3">
                     <div class="card">
                         <div class="card-inner">
                             <div class="card-front">
@@ -119,7 +119,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-3">
+                <div class="col-12 col-sm-6 col-md-3">
                     <div class="card">
                         <div class="card-inner">
                             <div class="card-front">
@@ -138,7 +138,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-3">
+                <div class="col-12 col-sm-6 col-md-3">
                     <div class="card">
                         <div class="card-inner">
                             <div class="card-front">
