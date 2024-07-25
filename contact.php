3a870819ebc328f4afca9897defafd51a9184989
sakatdata <47591291+sakatdata@users.noreply.github.com>
2024-07-25 18:39:37 -0400

    Added missing references

--- contact.php
+++ contact.php
@@ -25,6 +25,8 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+        <script src="js/ct_scripts.js"></script>
     </head>
 
     <body>
