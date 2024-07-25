72ac77927a5d1e250c3f1603a4969f32c45a77bd
Amber Fang <158085504+AmberFang112@users.noreply.github.com>
2024-07-25 16:38:13 -0400

    Add files via upload
    Adjust code to a mobile-friendly screen on July 25.

--- adapto.php
+++ adapto.php
@@ -9,10 +9,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/header-footer.css">
     <link rel="stylesheet" href="css/ad_style.css">
+    <link rel="stylesheet" href="css/styles.css">
 
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" defer></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" defer></script>
@@ -30,9 +31,9 @@
                 <h1 class="text-white mt-5 mb-lg-4" data-aos="zoom-in" data-aos-delay="800">
                     adapto™
                 </h1>
-                <h2 class="text-secondary-white-color" data-aos="fade-up" data-aos-delay="1000">
+                <p class="text-secondary-white-color" data-aos="fade-up" data-aos-delay="1000">
                     Modular Automation for a Dynamic Future 
-                </h2>
+                </p>
             </div>
 
             <div class="videoWrapper">
@@ -47,10 +48,10 @@
             <div class="container mb-5 pb-lg-5">
                 <div class="row justify-content-center">
                     <div class="col-lg-8 col-12">
-                        <h1 class="mb-3" data-aos="fade-up">Let us create...</h1>
-                        <h3 class="me-4" data-aos="fade-up" data-aos-delay="300">
+                        <h2 class="mb-3" data-aos="fade-up">Let us create...</h2>
+                        <p class="me-4" data-aos="fade-up" data-aos-delay="300">
                             Adapto® Smart Cell Technology by KONNEXIO is a customizable <strong>Lego-like</strong> automation system with modular smart cells. It allows quick adaptation to new processes and requirements, offering exceptional quality, efficiency, and flexibility for evolving customer needs.
-                        </h3>
+                        </p>
                     </div>
                 </div>
             </div>
@@ -64,21 +65,21 @@
                         <div class="row">
                             <div class="col-xl-4 col-lg-4 col-md-4 margin_bott">
                                 <div class="services_box">
-                                    <figure><img src="images/01.png" alt="#"/></figure>
+                                    <figure><img src="images/01.png" alt="#" class="img-fluid"/></figure>
                                     <h3>Modular Assembly</h3>
                                     <p>Smart Cells can function alone or be integrated into larger systems</p>
                                 </div>
                             </div>
                             <div class="col-xl-4 col-lg-4 col-md-4 margin_bott">
                                 <div class="services_box">
-                                    <figure><img src="images/adapto_02.png" alt="#"/></figure>
+                                    <figure><img src="images/adapto_02.png" alt="#" class="img-fluid"/></figure>
                                     <h3>Adaptive Processes</h3>
                                     <p>Easily add vision systems, feeders, and robots as needed.</p>
                                 </div>
                             </div>
                             <div class="col-xl-4 col-lg-4 col-md-4">
                                 <div class="services_box">
-                                    <figure><img src="images/adapto_03.png" alt="#"/></figure>
+                                    <figure><img src="images/adapto_03.png" alt="#" class="img-fluid"/></figure>
                                     <h3>Swift Modifications</h3>
                                     <p>Quickly adapt to changes, ensuring faster time-to-market.</p>
                                 </div>
