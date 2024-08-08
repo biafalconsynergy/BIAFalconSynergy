84b3c46a4ed287cb36d9cf134483db3c6a4269c1
Amber Fang <158085504+AmberFang112@users.noreply.github.com>
2024-08-07 20:03:12 -0400

    Update adapto.php 0807

--- adapto.php
+++ adapto.php
@@ -16,19 +16,19 @@
     <link rel="stylesheet" href="css/ad_style.css">
     <link rel="stylesheet" href="css/styles.css">
 
-<<<<<<< Updated upstream
+
     <!-- JS Libraries -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-=======
+
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" defer></script>
     <script src="js/ad_scripts.js" defer></script>
->>>>>>> Stashed changes
+
 </head>
 
 <body>
