de6adb218805b50d3a935a823080efeddbe51389
Laine <lalainebmac@gmail.com>
2024-08-11 09:31:16 -0400

    benefits draft

---
+++ benefits.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Benefits | KONNEXIO</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Konnexio is a custom machine builder located in London Ontario. Our specialty is adapto™ which is a modular assembly line construction technology.">
+    <link rel="icon" href="Logo/konnexio-icon.ico" type="image/x-icon">
+
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/be_styles.css">
+
+    <!-- JS Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    
+    <!-- Header Section -->
+    <?php include 'header.php'; ?>
+
+    <!-- Main -->
+    <div class="page-sec-main">
+        <h1>Benefits of adapto™</h1>
+    </div>
+    
+    <section class="container-fluid slide-up" style="padding: 0;">
+        <div class="row no-gutters" id="industrial">
+            <div class="col-sm-6 order-sm-first order-md-last">
+                <div class="be_sec bg-light">
+                    <img src="Images/benefits1.jpg" alt="Industrial Sector">
+                </div>
+            </div>
+            <div class="col-sm-6 order-sm-last order-md-first">
+                <div class="be_sec bg-light">
+                    <h2>Future Ready</h2>
+                    <h4>Evolving Market Demands</h4>
+                    <p>Traditional automation companies, require precise specifications upfront. This rigidity can create challenges when market demands shift or project specifications change, leading to scope creep, project delays, and inefficient investments. Adapto addresses these issues by offering a system designed to adapt to future uncertainties, ensuring that your manufacturing processes remain agile and responsive.</p>
+                    <h4>Innovative Features</h4>
+                    <p>Traditional automation companies, require precise specifications upfront. This rigidity can create challenges when market demands shift or project specifications change, leading to scope creep, project delays, and inefficient investments. Adapto addresses these issues by offering a system designed to adapt to future uncertainties, ensuring that your manufacturing processes remain agile and responsive.</p>
+                    <button class="btn btn-danger btn-lg" onclick="location.href='contact.php';">Book a Consultation</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <hr>
+    <section class="container-fluid slide-up" style="padding: 0;">
+        <div class="row no-gutters" id="agriculture">
+            <div class="col-sm-6 order-sm-first order-md-first">
+                <div class="be_sec bg-light">
+                    <img src="Images/Image2b.jpg" alt="Agriculture Sector">
+                </div>
+            </div>
+            <div class="col-sm-6 order-sm-last order-md-last">
+                <div class="be_sec bg-light">
+                    <h2>Bottleneck Optimization</h2>
+                    <h4>Over-automation and Under-automation</h4>
+                    <p>Many automation providers address bottlenecks by implementing large-scale automation solutions across entire production lines or extensive sections of the process. This method aims to enhance overall efficiency but often results in over-automation. These providers apply broad, generalized solutions without conducting a thorough analysis of which specific processes truly require automation. Consequently, manufacturers are burdened with the expense of automating non-critical stages while critical bottlenecks remain unaddressed. Additionally, such comprehensive systems are notoriously inflexible and costly to modify or expand as production needs evolve, leading to substantial initial investments and ongoing maintenance difficulties.</p>
+                    <button class="btn btn-danger btn-lg" onclick="location.href='contact.php';">Schedule a Demo</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Footer Section -->
+    <?php include 'footer.php'; ?>
+
+    <!-- JS -->
+    <script src="js/script.js"></script>
+    <script src="js/id_script.js"></script>
+</body>
+</html>
